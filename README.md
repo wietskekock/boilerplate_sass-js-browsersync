7a05e9de96bbd5cc769a771d55d4b3e039918326
AnnFell <annelottef@gmail.com>
2017-11-18 21:53:01 +0100

    Update js + vendors

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
-#README
+README
+------
 
-##Required
+Required
+--------
 - Node.js
 - Gulp: `npm install -g gulp`
 - Browsersync: `npm install -g browser-sync`
 
 
-###Start lokaal development: 
+Start lokaal development: 
+-------------------------
 
 Open folder in command line en typ `npm install`
 Dit installeert alle plugins in package.json onder devDependencies.
 
-###Gulp starten Open folder in command line en typ gulp
+Gulp starten:
+-------------
+Open folder in command line en typ `gulp` - server start op localhost:8080 met browsersync
 
-Server start op localhost:8080, voor gulp connect met livereload
-
-###Notes Nieuwe package installeren (deze voeg je hiermee ook toe onder devDependecies in package.json):
-
-npm install ---save-dev package (vervang package met naam van package)
+Notes
+----- 
+Nieuwe package installeren (deze voeg je hiermee ook toe onder devDependecies in package.json):
+`npm install ---save-dev package` (vervang package met naam van package)
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat');
 
-//https://www.browsersync.io/docs/gulp#gulp-sass-css
+var cssSources,
+    jsSources;
+
+cssSources = ['sass/*.scss']
+jsSources = [
+    'js/vendor/jquery-3.2.1.min.js',
+    'vendor/gsap/TimelineMax.min.js',
+    'js/vendor/scrollmagic/uncompressed/ScrollMagic.js',
+    'js/vendor/scrollmagic/uncompressed/plugins/debug.addIndicators.js',
+    'js/main.js'
+];
+
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -13,16 +26,28 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch("gulp/sass/*.scss", ['sass']);
+    gulp.watch("gulp/js/*.js", ['scripts']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("gulp/sass/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("../"))
+// concat js files
+gulp.task('scripts', function() {
+    return gulp.src(jsSources)
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(sourcemaps.write('./maps/'))
+        .pipe(gulp.dest('../js/'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(cssSources)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps/'))
+        .pipe(gulp.dest("../css/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['scripts', 'serve']);
